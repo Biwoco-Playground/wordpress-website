cdbb9d2a1d5a40033f0fb7bedc9aacd391ff7091
nguyenvl179 <nhoxzznhox16@gmail.com>
2021-06-24 11:49:45 +0700

    Chap 7 develop wordpress plugin

--- app/html/wp-content/plugins/ch2-private-item-text/index.php
+++ app/html/wp-content/plugins/ch2-private-item-text/index.php
@@ -15,7 +15,28 @@ License: GPLv2
 add_action('admin_menu', 'ch2pit_settings_menu');
 add_action('admin_init', 'ch2pit_admin_init');
 add_action('wp_head', 'ch2pit_page_header_output');
+add_action('user_new_form', 'ch2pit_show_user_profile');
+add_action('edit_user_profile', 'ch2pit_show_user_profile');
+add_action('user_register', 'ch2pit_save_user_data');
+add_action('profile_update', 'ch2pit_save_user_data');
+add_action('restrict_manage_users', 'ch2pit_add_user_filter');
+add_action('admin_footer', 'ch2pit_user_filter_js');
 
+
+add_filter('manage_users_columns', 'ch2pit_add_user_columns');
+add_filter(
+    'manage_users_custom_column',
+    'ch2pit_display_user_columns_data',
+    10,
+    3
+);
+add_filter('pre_get_users', 'ch2pit_filter_users');
+
+
+add_shortcode('paid', 'ch2pit_paid_shortcode');
+
+global $user_levels;
+$user_levels = array('regular' => 'Regular', 'paid' => 'Paid');
 
 function ch2pit_page_header_output()
 { ?>
@@ -147,4 +168,167 @@ function process_ch2pit_options()
         admin_url('options-general.php')
     ));
     exit;
+}
+
+function ch2pit_show_user_profile($user)
+{
+    global $user_levels;
+    if ('add-new-user' == $user) {
+        $current_user_level = '';
+    } elseif (!empty($user) && isset($user->data->ID)) {
+        $user_id = $user->data->ID;
+        $current_user_level = get_user_meta($user_id, 'user_level', true);
+    } ?>
+    <h3>Site membership level</h3>
+    <table class="form-table">
+        <tr>
+            <th>Level</th>
+            <td><select name="user_level" id="user_level">
+                    <?php foreach ($user_levels as
+                        $user_level_index => $user_level) { ?>
+                        <option value="<?php echo $user_level_index; ?>" <?php selected(
+                                                                                $current_user_level,
+                                                                                $user_level_index
+                                                                            ); ?>>
+                            <?php echo $user_level; ?></option>
+                    <?php } ?>
+                </select></td>
+        </tr>
+    </table>
+<?php }
+
+
+function ch2pit_save_user_data($user_id)
+{
+    global $user_levels;
+    if (
+        isset($_POST['user_level']) &&
+        !empty($_POST['user_level']) &&
+        array_key_exists(
+            $_POST['user_level'],
+            $user_levels
+        )
+    ) {
+        update_user_meta(
+            $user_id,
+            'user_level',
+            $_POST['user_level']
+        );
+    } else {
+        update_user_meta($user_id, 'user_level', 'regular');
+    }
+}
+
+function ch2pit_add_user_columns($columns)
+{
+    $new_columns = array_slice($columns, 0, 2, true) +
+        array('level' => 'User Level') +
+        array_slice($columns, 2, NULL, true);
+    return $new_columns;
+}
+
+function ch2pit_display_user_columns_data(
+    $val,
+    $column_name,
+    $user_id
+) {
+    global $user_levels;
+    if ('level' == $column_name) {
+        $current_user_level = get_user_meta(
+            $user_id,
+            'user_level',
+            true
+        );
+        if (!empty($current_user_level)) {
+            $val = $user_levels[$current_user_level];
+        }
+    }
+    return $val;
+}
+
+function ch2pit_add_user_filter()
+{
+    global $user_levels;
+    $filter_value = '';
+    if (isset($_GET['user_level'])) {
+        $filter_value = $_GET['user_level'];
+    } ?>
+    <select name="user_level" class="user_level" style="float:none;">
+        <option value="">No filter</option>
+        <?php foreach ($user_levels as
+            $user_level_index => $user_level) { ?>
+            <option value="<?php echo $user_level_index; ?>" <?php selected($filter_value, $user_level_index); ?>>
+                <?php echo $user_level; ?></option>
+        <?php } ?>
+        <input type="submit" class="button" value="Filter">
+    <?php }
+
+function ch2pit_user_filter_js()
+{
+    global $current_screen;
+    if ('users' != $current_screen->id) {
+        return;
+    } ?>
+        <script type="text/javascript">
+            jQuery(document).ready(function() {
+                jQuery('.user_level').first().change(function() {
+                    jQuery('.user_level').
+                    last().val(jQuery(this).val());
+                });
+                jQuery('.user_level').last().change(function() {
+                    jQuery('.user_level').
+                    first().val(jQuery(this).val());
+                });
+            });
+        </script>
+    <?php }
+
+function ch2pit_filter_users($query)
+{
+    global $pagenow;
+    global $user_levels;
+    if (
+        is_admin() && 'users.php' == $pagenow &&
+        isset($_GET['user_level'])
+    ) {
+        $filter_value = $_GET['user_level'];
+        if (
+            !empty($filter_value) &&
+            array_key_exists(
+                $_GET['user_level'],
+                $user_levels
+            )
+        ) {
+            $query->set('meta_key', 'user_level');
+            $query->set('meta_query', array(
+                array(
+                    'key' => 'user_level',
+                    'value' => $filter_value
+                )
+            ));
+        }
+    }
+}
+
+
+function ch2pit_paid_shortcode($atts, $content = null)
+{
+    if (is_user_logged_in()) {
+        $current_user = wp_get_current_user();
+        $current_user_level = get_user_meta(
+            $current_user->ID,
+            'user_level',
+            true
+        );
+        if (
+            'paid' == $current_user_level ||
+            current_user_can('activate_plugins')
+        ) {
+            return '<div class="paid">' . $content . '</div>';
+        }
+    }
+    $output = '<div class="register">';
+    $output .= 'You need to be a paid member to access ';
+    $output .= 'this content.</div>';
+    return $output;
 }
