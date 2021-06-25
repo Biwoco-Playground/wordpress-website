acf03d59c6e0ec58bf54b03a43cab06c69a78a4a
nguyenvl179 <nhoxzznhox16@gmail.com>
2021-06-25 17:02:40 +0700

    Format code Wordpress Plugin

--- app/html/wp-content/plugins/crawl-hacker-new/crawl-hacker-new.php
+++ app/html/wp-content/plugins/crawl-hacker-new/crawl-hacker-new.php
@@ -10,28 +10,30 @@ Author: Phuc Nguyen
 License: GPLv2
 */
 
-add_shortcode( "crawl_hacker_new", "crawl_hacker_new" );
+add_shortcode("crawl_hacker_new", "crawl_hacker_new");
 
+function crawl_hacker_new()
+{
+    $curl = curl_init('https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty');
 
-function crawl_hacker_new () {
-    $curl = curl_init( 'https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty' );
     curl_setopt($curl, CURLOPT_HTTPGET, true);
-    
+
     curl_setopt($curl, CURLOPT_HTTPHEADER, array(
         'Content-Type: application/json',
-        'Accept: application/json'
+        'Accept: application/json',
+        "cache-control: no-cache"
     ));
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($curl, CURLOPT_ENCODING, 'gzip');
-    
     $result = curl_exec($curl);
-    
     curl_close($curl);
 
-    $newsObj = json_decode($result, true);
+    $newsArr = json_decode($result, true);
 
-    echo "<table>";
-    echo "<tr>
+    $output = "";
+
+    $output .= "<table>";
+    $output .= "<tr>
             <th>ID</th>
             <th>Title</th>
             <th>Type</th>
@@ -39,50 +41,49 @@ function crawl_hacker_new () {
             <th>Score</th>
         </tr>";
 
-    for($i = 0; $i<10; $i++){
-        getNew($newsObj[$i]);
+    for ($i = 0; $i < 10; $i++) {
+        $output .= getNew($newsArr[$i]);
     }
 
-    echo "</table>";
+    $output .= "</table>";
+
+    return $output;
 }
 
-function getNew($id) {
-    $curl = curl_init( 'https://hacker-news.firebaseio.com/v0/item/'. $id .'.json?print=pretty' );
+
+
+function getNew($id)
+{
+    $curl = curl_init('https://hacker-news.firebaseio.com/v0/item/' . $id . '.json?print=pretty');
+
     curl_setopt($curl, CURLOPT_HTTPGET, true);
-    
     curl_setopt($curl, CURLOPT_HTTPHEADER, array(
         'Content-Type: application/json',
-        'Accept: application/json'
+        'Accept: application/json',
+        "cache-control: no-cache"
     ));
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($curl, CURLOPT_ENCODING, 'gzip');
-    
+
     $result = curl_exec($curl);
-    
+
     curl_close($curl);
 
-    $newsObj = json_decode($result, true);
+    $newObj = json_decode($result, true);
 
-    echo "<tr>";
-    echo "<td>";
-    echo $newsObj["id"];
-    echo "</td>"; 
-    echo "<td>";
+    $output =  "<tr>";
+    $output .= "<td>{$newObj["id"]}</td>";
+    $output .= "<td>";
 
-    if(isset($newsObj["url"]))
-        echo "<a href='{$newsObj["url"]}'>{$newsObj["title"]}</a>";
-    else echo $newsObj["title"];
-    echo "<div>Created at: " . date("m/d/Y H:i:s", $newsObj["time"]) . "</div>";
-    echo "</td>";    
-    echo "<td>";
-    echo $newsObj["type"];
-    echo "</td>";    
-    echo "<td>";
-    echo $newsObj["by"];
-    echo "</td>";    
-    echo "<td>";
-    echo $newsObj["score"];
-    echo "</td>";      
-    echo "</tr>";
+    if (isset($newObj["url"]))
+        $output .= "<a href='{$newObj["url"]}'>{$newObj["title"]}</a>";
+    else $output .= $newObj["title"];
 
+    $output .= "<div>Created at: " . date("m/d/Y H:i:s", $newObj["time"]) . "</div></td>";
+    $output .= "<td>{$newObj["type"]}</td>";
+    $output .= "<td>{$newObj["by"]}</td>";
+    $output .= "<td>{$newObj["score"]}</td>";
+    $output .= "</tr>";
+
+    return $output;
 }
